1e66b8d202e7cc0719e1bcd023ca1308df66f1f4
Pratik-010 <138128208+Pratik-010@users.noreply.github.com>
2023-08-23 16:51:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-
-echo "Hello, Welcome to SUNBEAM <br>";
-echo "PG-DITISS! <br>";
-echo "Hinjewadi,PUNE";
-
+// get-file.php
+define('PDF_DIR','/home/the_actual_physical_path/documents/pdfs/');
+require_once('../mysql_connect.php');
+$id = intval($_GET['id']);
+$sql = "SELECT id, pdf FROM pdf_table WHERE id = '$id'";
+$res = mysql_query($sql);
+$row = mysql_fetch_array($res);
+if (!empty($row['1'])) {
+ header("Content-type: application/pdf");
+ header("Content-Disposition: attachment;filename=" . $row['pdf']);
+ readfile( PDF_DIR . $row['pdf']);
+}
+else {
+ $referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+ header("Location: $referer");
+}
 ?>
