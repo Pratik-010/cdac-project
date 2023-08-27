2fb6c532d0316da73dc345567b0021ef4b573f59
Pratik-010 <138128208+Pratik-010@users.noreply.github.com>
2023-08-27 13:44:30 +0530

    Add files via upload

--- news-details.php
+++ news-details.php
@@ -10,15 +10,20 @@ $query=mysqli_query($con,"SELECT * FROM students WHERE StudentRegno='$regno' and
 $num=mysqli_fetch_array($query);
 if($num>0)
 {
+$extra="change-password.php";//
 $_SESSION['login']=$_POST['regno'];
 $_SESSION['id']=$num['studentRegno'];
 $_SESSION['sname']=$num['studentName'];
 $uip=$_SERVER['REMOTE_ADDR'];
 $status=1;
 $log=mysqli_query($con,"insert into userlog(studentRegno,userip,status) values('".$_SESSION['login']."','$uip','$status')");
-header("location:http:change-password.php");
-}else{
+}
+else
+{
 $_SESSION['errmsg']="Invalid Reg no or Password";
+$extra="index.php";
+$host  = $_SERVER['HTTP_HOST'];
+$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
 header("location:http:index.php");
 }
 }
@@ -64,39 +69,27 @@ header("location:http:index.php");
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h4 class="page-head-line">Please Login To Enter </h4>
+                    <h4 class="page-head-line">News Details </h4>
 
                 </div>
 
             </div>
-             <span style="color:red;" ><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg']="");?></span>
-            <form name="admin" method="post">
-            <div class="row">
-                <div class="col-md-6">
-                     <label>Enter Reg no : </label>
-                        <input type="text" name="regno" class="form-control"  />
-                        <label>Enter Password :  </label>
-                        <input type="password" name="password" class="form-control"  />
-                        <hr />
-                        <button type="submit" name="submit" class="btn btn-info"><span class="glyphicon glyphicon-user"></span> &nbsp;Log Me In </button>&nbsp;
-                </div>
-                </form>
-                <div class="col-md-6">
+
+
+                <div class="col-md-12">
                     <div class="alert alert-info">
-                
-                         <strong> Latest News / Updates</strong>
-                         <marquee direction='up'  scrollamount="2" onmouseover="this.stop();" onmouseout="this.start();">
+        
                         <ul>
-                            <?php
-$sql=mysqli_query($con,"select * from news");
+<?php $nid=$_GET['nid'];
+$sql=mysqli_query($con,"select * from news where id='$nid'");
 $cnt=1;
 while($row=mysqli_fetch_array($sql))
-{
-?>
-                            <li>
-                              <a href="news-details.php?nid=<?php echo htmlentities($row['id']);?>"><?php echo htmlentities($row['newstitle']);?>-<?php echo htmlentities($row['postingDate']);?></a>
-                            </li>
-                           <?php } ?> 
+{?>
+<h3><?php echo htmlentities($row['newstitle']);?></h3>
+<small><?php echo htmlentities($row['postingDate']);?></small>
+<hr />   
+<p><?php echo htmlentities($row['newsDescription']);?></p>                  
+<?php } ?> 
                      
                         </ul>
                     </marquee>
